891e25cb9ba1d4b151362a7d2838411486cd7003
Oliver Saxon <oliverrsaxon@gmail.com>
2024-01-18 16:43:59 +0000

    fixes new user page

--- src/app/auth/new-user/page.tsx
+++ src/app/auth/new-user/page.tsx
@@ -1,6 +1,5 @@
 import Container from "@/components/container";
 import { UserProfileform } from "@/components/forms/user-profile-form";
-import ProfileImage from "@/components/profile-image";
 import { AuthenticationError, auth } from "@/libs/authconfig";
 
 export default async function NewUserPage() {
@@ -15,8 +14,6 @@ export default async function NewUserPage() {
         <Container className="space-y-8" width="wide" height="page">
             <h2 className="font-bold text-2xl">Please complete your profile</h2>
             <section className="space-y-8">
-                <ProfileImage session={session} />
-
                 <UserProfileform session={session} />
             </section>
         </Container>
